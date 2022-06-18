ea9648021d9bfeb549d5dc7e713d5fdfd95cbe2d
Em <contactemilyhuang@gmail.com>
2022-06-17 20:59:07 -0700

    Add structure and styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+const categories = ['category 1', 'category 2'];
+const items = [
+  {
+    productname: 'item name',
+    id: '12345',
+    category: 'category 1',
+    price: '$5.00',
+    stock: 99,
+    imgsrc: '/',
+    imgalt: 'no image'
+  },
+  {
+    productname: 'item name',
+    id: '12345',
+    category: 'category 1',
+    price: '$5.00',
+    stock: 99,
+    imgsrc: '/',
+    imgalt: 'no image'
+  },
+  {
+    productname: 'item name',
+    id: '12345',
+    category: 'category 1',
+    price: '$5.00',
+    stock: 99,
+    imgsrc: '/',
+    imgalt: 'no image'
+  }
+];
+
+const itemsLength = items.length;
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    categories: categories,
+    items: items,
+    number_of_items: itemsLength
+  });
 });
 
 module.exports = router;
